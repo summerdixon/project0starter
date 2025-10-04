db6ac87505f4b47d7570a464eac9fa9351f42f57
summerdixon <summerdixon2023@g.ucla.edu>
2025-10-04 05:13:37 -0700

    Initial implementation

--- project/client.c
+++ project/client.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -18,12 +19,30 @@ int main(int argc, char** argv) {
     int port = atoi(argv[2]);
 
     // TODO: Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     // TODO: Set stdin and socket nonblocking
+    int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);
 
     // TODO: Construct server address
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    int PORT = 8080;
+    serveraddr.sin_port = htons(PORT);
 
-    char buffer[1024];
+    char client_buf[] = "Hello world!";
+    int did_send = sendto(sockfd, client_buf, strlen(client_buf),
+                      0, (struct sockaddr*) &serveraddr,
+                      sizeof(serveraddr));
+    if (did_send < 0) return errno;
+
+    int BUF_SIZE = 1024;
+    char server_buf[BUF_SIZE];
+    socklen_t serversize = sizeof(struct sockaddr_in);
 
     // Listen loop
     while (1) {
@@ -31,6 +50,12 @@ int main(int argc, char** argv) {
         // TODO: If data, write to stdout
         // TODO: Read from stdin
         // TODO: If data, send to socket
+
+        int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 
+                           0, (struct sockaddr*) &serveraddr, 
+                           &serversize);
+        if (bytes_recvd < 0) return errno;
+        write(1, server_buf, bytes_recvd);
     }
 
     return 0;
--- project/server.c
+++ project/server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -16,25 +17,47 @@ int main(int argc, char** argv) {
     int port = atoi(argv[1]);
 
     // TODO: Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     // TODO: Set stdin and socket nonblocking
+    int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);
 
     // TODO: Construct server address
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int PORT = 8080;
+    servaddr.sin_port = htons(PORT);
 
     // TODO: Bind address to socket
+    int did_bind = bind(sockfd, (struct sockaddr*) &servaddr, 
+                    sizeof(servaddr));
+    if (did_bind < 0) return errno;
 
     // TODO: Create sockaddr_in and socklen_t buffers to store client address
+    int BUF_SIZE = 1024;
+    char client_buf[BUF_SIZE];
+    struct sockaddr_in clientaddr = {0}; 
+    socklen_t clientsize = sizeof(clientaddr);
 
-    char buffer[1024];
     int client_connected = 0;
 
     // Listen loop
     while (1) {
         // TODO: Receive from socket
+        int bytes_recvd = recvfrom(sockfd, client_buf, BUF_SIZE, 
+                           0, (struct sockaddr*) &clientaddr, 
+                           &clientsize);
         // TODO: If no data and client not connected, continue
+        if (bytes_recvd <= 0) continue;
         // TODO: If data, client is connected and write to stdout
         // TODO: Read from stdin
         // TODO: If data, send to socket
+
+        if (bytes_recvd < 0) return errno;
     }
 
     return 0;
